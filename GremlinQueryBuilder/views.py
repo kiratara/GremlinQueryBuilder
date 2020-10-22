48b1b9e9dfaa8c358558ee2d6e6d688baccf4c2e
kiratara <raibinita217@gmail.com>
2020-10-21 20:36:18 -0500

    add new route

--- GremlinQueryBuilder/views.py
+++ GremlinQueryBuilder/views.py
@@ -10,14 +10,40 @@ Contains following views/functions:
 
 
 from GremlinQueryBuilder import app
-from flask import render_template
+from flask import render_template, request, jsonify, make_response
+import pandas as pd
+from .helper_functions import get_edge_df, get_vertex_df
 
 
 @app.route('/')
 def home():
+    '''renders the home page'''
     return render_template('home.html')
 
 
 @app.route('/about')
 def about():
+    '''render about page'''
     return render_template('about.html')
+
+
+@app.route('/get_query', methods=["POST", "GET"])
+def basic_query_selection():
+    '''method queries query based on selection'''
+    req = request.get_json()  # convert received json to python dictionary
+    entity = req['entity']
+    property_name = req['property_name']
+    property_value = req['property_value']
+
+    # get the correct dataframe to 
+    if entity == "V()":
+        df = get_vertex_df()
+    else:
+        df = get_edge_df()
+    
+    # extract actual data based on property_name and value
+    result = df[df[property_name] == property_value]
+    print(f'{df} \n {entity} {property_name} {property_value}')
+    print(result)
+    response = make_response(jsonify(req), 200)
+    return response
